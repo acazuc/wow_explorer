4d98f8f637360eeab98a1402f703afc64ca261ca
acazuc <acazuc@acazuc.fr>
2023-01-25 20:08:53 +0100

    txt: non editable

--- src/displays/txt.c
+++ src/displays/txt.c
@@ -22,6 +22,7 @@ struct display *txt_display_new(const struct node *node, const char *path, struc
 	gtk_text_buffer_set_text(buffer, (const char*)file->data, file->size);
 	GtkWidget *text = gtk_text_view_new_with_buffer(buffer);
 	gtk_text_view_set_monospace(GTK_TEXT_VIEW(text), true);
+	gtk_text_view_set_editable(GTK_TEXT_VIEW(text), false);
 	gtk_widget_show(text);
 	GtkWidget *scrolled = gtk_scrolled_window_new(NULL, NULL);
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
